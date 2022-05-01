357a9edc300ff22594638eefd61ae204ff47503b
nonari <17324278+nonari@users.noreply.github.com>
2022-05-01 02:50:33 +0200

    unet

--- part2/P2/ex2/unet_modules_padding.py
+++ part2/P2/ex2/unet_modules_padding.py
@@ -7,9 +7,9 @@ class ConvolveTwice(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), padding=1, bias=False),
             nn.ReLU()
         )
 
@@ -22,7 +22,7 @@ class Up(nn.Module):
         super().__init__()
         out_channels = in_channels // 2
         self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.conv_up = nn.Conv2d(in_channels, out_channels, kernel_size=(2, 2))
+        self.conv_up = nn.Conv2d(in_channels, out_channels, padding='same', kernel_size=(2, 2))
         self.conv = ConvolveTwice(in_channels, out_channels)
 
     def forward(self, x_r, x_l):
@@ -30,8 +30,8 @@ class Up(nn.Module):
         x_r = self.conv_up(x_r)
         diff_y = x_l.size()[2] - x_r.size()[2]
         diff_x = x_l.size()[3] - x_r.size()[3]
-
-        x_r = x_r[:, :, diff_x // 2:-diff_x // 2, diff_y // 2, -diff_y // 2]
+        x_r = F.pad(x_r, [diff_x // 2, diff_x - diff_x // 2,
+                          diff_y // 2, diff_y - diff_y // 2])
         x = torch.cat([x_l, x_r], dim=1)
         return self.conv(x)
 
