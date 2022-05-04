b1b7d59ff4710036088c289dbdf4ec8697a1e6fa
nonari <17324278+nonari@users.noreply.github.com>
2022-05-04 19:34:05 +0200

    fix

--- part2/P2/ex2/tester.py
+++ part2/P2/ex2/tester.py
@@ -5,7 +5,9 @@ from pytorch_lightning.loggers import TensorBoardLogger
 from datasets import DataModule
 from models import GenericNet
 import torch
-
+import numpy as np
+from sklearn.metrics import roc_curve, confusion_matrix, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
 
 
 def test(config_name, check_path=None, batch=4, version=0):
@@ -23,7 +25,7 @@ def test(config_name, check_path=None, batch=4, version=0):
 
     model_config = CONFIG['model']
 
-    device = torch.device('cpu') if torch.cuda.is_available() else torch.device('cpu')
+    device = torch.device('gpu') if torch.cuda.is_available() else torch.device('cpu')
 
     model = model_config['class'](**model_config['params'])
 
@@ -38,23 +40,43 @@ def test(config_name, check_path=None, batch=4, version=0):
     iterations = 0
     val_accuracy = 0
     datapoints = 0
+    softmax = torch.nn.Softmax()
+    all_targets = np.zeros((0, 1, 128, 128))
+    all_preds = np.zeros((0, 11, 128, 128))
     for images, targets in dataset.test_dataloader():
         images = images.to(device)
         targets = targets.to(device)
 
         with torch.no_grad():
             logits = model(images)
-
+            logits = logits[:, :, 2:-2, 2:-2]
             iterations += 1
 
             preds = torch.argmax(logits, dim=1).flatten()
             datapoints += preds.shape[0]
-            val_accuracy += (preds == targets.flatten()).sum().item()
+            val_accuracy += (preds.flatten() == targets.flatten()).sum().item()
+            all_preds = np.concatenate((all_preds, softmax(logits).detach().numpy()))
+            all_targets = np.concatenate((all_targets, targets.detach().numpy()))
 
     print()
 
     val_accuracy /= datapoints
     print(val_accuracy)
 
+    for c in range(11):
+        fpr, tpr, t = roc_curve(np.int0(all_targets.flatten() == c), all_preds[:, c].flatten())
+        plt.plot(fpr, tpr)
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.axline((0, 0), (1, 1), color='blue', linestyle='dashed')
+        plt.title(f'Class: {c if c > 0 else "Background"}')
+        plt.grid('on')
+        plt.show()
 
-# test('resnet18_pre_aug', version=0)
+    cm = confusion_matrix(all_targets.flatten(), np.argmax(all_preds, axis=1).flatten())
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.show()
+
+
+# test('unet_simple', version=0)
